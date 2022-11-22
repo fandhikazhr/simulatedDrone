08724cfc8d02aeda91ce6a654480b2eba2cda675
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-11-23 02:55:22 +0700

    Update takeoffmissionland.py

--- pythonScript/takeoffmissionland.py
+++ pythonScript/takeoffmissionland.py
@@ -7,8 +7,11 @@ import sys
 master = mavutil.mavlink_connection('udpin:127.0.0.1:14552')
 master.wait_heartbeat()
 
-# Choose Flight Mode
+mode0 = 'STABILIZE'
 mode1 = 'GUIDED'
+mode3 = 'AUTO'
+mode4 = 'RTL'
+mode5 = 'LOITER'
 
 mode_id = master.mode_mapping()[mode1]
 
@@ -25,13 +28,12 @@ print('Connected to vehicle on: %s' % args.connect)
 vehicle = connect(args.connect, baud=57600, wait_ready=True)
 
 def arm_and_takeoff(aTargetAltitude):
-  print "Basic pre-arm checks"
+  print("Basic pre-arm checks")
   while not vehicle.is_armable:
     print('Waiting for vehicle to initialise ...')
     time.sleep(1)
     
   print('Arming motors')
-  vehicle.mode = VehicleMode("GUIDED")
   vehicle.armed = True
   
   while not vehicle.armed:
@@ -44,35 +46,38 @@ def arm_and_takeoff(aTargetAltitude):
   while True:
     print("=> Altitude : ", vehicle.location.global_relative_frame.alt)
     if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
-      print "Reached target altitude !!!"
+      print("Reached target altitude !!!")
       break
     time.sleep(1)
 
 # start initial takeoff
 arm_and_takeoff(10)
-time.sleep(5)
-
+time.sleep(3)
 print("Takeoff Successfully !!!")
 
 # hovering 15 seconds
+print("Hovering !!!")
 time.sleep(15)
 
 # Mission Started
+print("--> Going to Point 1")
 point1 = LocationGlobalRelative(-35.361354, 149.165218, 10)
 vehicle.simple_goto(point1)
-print("--> Going to Point 1")
 
-time.sleep(5)
+time.sleep(30)
 
+print("--> Going to Point 2")
 point2 = LocationGlobalRelative(-35.363244, 149.168801, 10)
 vehicle.simple_goto(point2)
-print("--> Going to Point 2")
 
-time.sleep(5)
+time.sleep(30)
 print("Mission Completed !!!")
 
 # hovering 15 seconds
+print("Hovering before land !!!")
 time.sleep(15)
+
+vehicle.close()
 
 # Change Flight Mode for Landing
 mode2 = 'LAND'
@@ -85,5 +90,4 @@ master.mav.set_mode_send(
   mode_land)
 
 print("Landing ...")
-
-vehicle.close()
+print("Land Successfully !!!")
