e927e1410cdf85ff0b7779b8c00fed8c8cd492e5
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-11-22 19:22:37 +0700

    Update changeflightmode.py

--- pythonScript/changeflightmode.py
+++ pythonScript/changeflightmode.py
@@ -4,10 +4,14 @@ from pymavlink import mavutil
 master = mavutil.mavlink_connection('udpin:127.0.0.1:14550')
 master.wait_heartbeat()
 
+mode0 = 'STABILIZE'
 mode1 = 'GUIDED'
 mode2 = 'LAND'
+mode3 = 'AUTO'
+mode4 = 'RTL'
+mode5 = 'LOITER'
 
-mode_id = master.mode_mapping()[mode]
+mode_id = master.mode_mapping()[#choose_mode]
 
 master.mav.set_mode_send(
   master.target_system,
